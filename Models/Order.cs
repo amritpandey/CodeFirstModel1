c11f1a1c5421a89945ce0e60b3e5b0f8fc45ced1
amritpandey <amrit.pandey111@gmail.com>
2020-09-20 00:37:07 +0200

    add code first model

---
+++ Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace udemyCodeFirst.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        public int OrderID { get; set; }
+        [ForeignKey("Customer")]
+        public int CustomerID { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public virtual ICollection<OrderItem> Items { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
